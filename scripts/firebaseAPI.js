fa1a242bd16beec1485d3c227f2d2044012970f4
Andy <90529129+Nandddy@users.noreply.github.com>
2021-11-01 18:36:08 -0700

    Update firebaseAPI.js

--- scripts/firebaseAPI.js
+++ scripts/firebaseAPI.js
@@ -1,11 +1,11 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyBffDsBfqqnrBnye0R3QEUgnv33nQ4xy24",
-    authDomain: "fir-comp1800-5015b.firebaseapp.com",
-    projectId: "fir-comp1800-5015b",
-    storageBucket: "fir-comp1800-5015b.appspot.com",
-    messagingSenderId: "351790299954",
-    appId: "1:351790299954:web:47675fcb93a403828a752b"
+    apiKey:
+    authDomain: 
+    projectId:
+    storageBucket:
+    messagingSenderId:
+    appId:
   };
   
   
