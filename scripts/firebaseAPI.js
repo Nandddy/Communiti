b7814cf9593a226d66fceb5de412a3daf129f5bf
Andy Nguyen <andy-nguyen@live.ca>
2021-11-01 17:47:03 -0700

    Changed sign-in page to use Firebase

---
+++ scripts/firebaseAPI.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBffDsBfqqnrBnye0R3QEUgnv33nQ4xy24",
+    authDomain: "fir-comp1800-5015b.firebaseapp.com",
+    projectId: "fir-comp1800-5015b",
+    storageBucket: "fir-comp1800-5015b.appspot.com",
+    messagingSenderId: "351790299954",
+    appId: "1:351790299954:web:47675fcb93a403828a752b"
+  };
+  
+  
+  const app = firebase.initializeApp(firebaseConfig);
+  
