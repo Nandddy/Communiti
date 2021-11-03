0ad9cd2fac43e9a078e622d778ffe753c7d1b3e1
Andy <andy-nguyen@live.ca>
2021-11-03 09:54:44 -0700

    login.html now writes new users to db

--- scripts/firebaseAPI.js
+++ scripts/firebaseAPI.js
@@ -1,13 +1,13 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey:
-    authDomain: 
-    projectId:
-    storageBucket:
-    messagingSenderId:
-    appId:
-  };
-  
-  
-  const app = firebase.initializeApp(firebaseConfig);
-  
+  apiKey: "AIzaSyD4oV7-ExmuvH0i53_S-y-MSgLrt2SKG0c",
+  authDomain: "communiti-4ea70.firebaseapp.com",
+  projectId: "communiti-4ea70",
+  storageBucket: "communiti-4ea70.appspot.com",
+  messagingSenderId: "229395775259",
+  appId: "1:229395775259:web:d5ddd528406bc102751dbe"
+};
+
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
