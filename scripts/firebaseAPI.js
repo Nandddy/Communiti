f43578c706cf0a5a6418d3e83a1031fbe1231cee
Andy <90529129+Nandddy@users.noreply.github.com>
2021-11-03 10:20:36 -0700

    Update firebaseAPI.js

--- scripts/firebaseAPI.js
+++ scripts/firebaseAPI.js
@@ -1,11 +1,6 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD4oV7-ExmuvH0i53_S-y-MSgLrt2SKG0c",
-  authDomain: "communiti-4ea70.firebaseapp.com",
-  projectId: "communiti-4ea70",
-  storageBucket: "communiti-4ea70.appspot.com",
-  messagingSenderId: "229395775259",
-  appId: "1:229395775259:web:d5ddd528406bc102751dbe"
+//please stop
 };
 
 
